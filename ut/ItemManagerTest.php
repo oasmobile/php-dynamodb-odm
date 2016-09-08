288b880fdd2005b3eec3fefd71f51905e58e78a9
Minhao Zhang <minhao.zhang@qq.com>
2016-09-08 20:58:02 +0800

    + itemManager ut

---
+++ ut/ItemManagerTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: minhao
+ * Date: 2016-09-08
+ * Time: 20:56
+ */
+
+namespace Oasis\Mlib\ODM\Dynamodb\Ut;
+
+class ItemManagerTest extends \PHPUnit_Framework_TestCase
+{
+    
+}
